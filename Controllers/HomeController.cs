52ae791b4d496ccdc074c1130d5e3e40effce1bb
AdrianDarius2d <fanica.darius.s2d@student.ucv.ro>
2026-04-06 10:51:25 +0300

    Update Index.cshtml (#3)
    * Update Index.cshtml
    
    * Update HomeController.cs
    
    * fix HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace FlightManagementSystem.Controllers
         public IActionResult Index()
         {
             return View();
+          
         }
 
         public IActionResult Privacy()
